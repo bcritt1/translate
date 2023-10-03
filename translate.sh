82ca29f70f218b31fff44b4b0b2a55b5850f8c9d
bcritt1 <bcritt@stanford.edu>
2023-10-03 12:16:37 -0700

    add error handling by appending rand nums to outputs

--- translate.sh
+++ translate.sh
@@ -8,18 +8,18 @@ wget git.io/trans
 chmod +x ./trans
 for f in $FILES;
 do
-  	tiff=${f%.*}.tiff
+        tiff=${f%.*}.tiff
         convert -density 400 $f -depth 8 -strip -background white -alpha off $tiff
         ocr=${f%.*}_ocr
         tlate=${f%.*}_trans
-	tesseract $tiff $ocr -l rus
-	./trans :en file://$ocr.txt > $tlate.txt
-	sleep 1m
+        tesseract $tiff $ocr -l rus
+        ./trans :en file://$ocr.txt > $tlate.txt
+        sleep 1m
 done
-
-mkdir $DIR"/ocr"
-mkdir $DIR"/tiff"
-mkdir $DIR"/tlate"
-mv $DIR/*_ocr.txt $DIR/ocr
-mv $DIR/*.tiff $DIR/tiff
-mv $DIR/*_trans.txt $DIR/tlate
+randnum="$RANDOM"
+mkdir $DIR/"${randnum}_ocr"
+mkdir $DIR/"${randnum}_tiff"
+mkdir $DIR/"${randnum}_tlate"
+mv $DIR/*_ocr.txt $DIR/${randnum}"_ocr"
+mv $DIR/*.tiff $DIR/${randnum}"_tiff"
+mv $DIR/*_trans.txt $DIR/${randnum}"_tlate"
